73b4e33d2b8e757000ca983c8678672ca18a6fed
abdulhaseebss <m.saleemhaseeb@gmail.com>
2024-05-04 09:52:14 +0500

    Navbar progress

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,36 @@ import logo from "../../assets/logo.png"
 const Navbar = () => {
   return (
     <>
-        <div>
+
+
+    {/* navbar start */}
+
+
+    <nav className='flex justify-center'>
+        <div className='list-none flex bg-[#0065C0] text-white items-center justify-between px-8 p-2 w-[90%] mt-6 rounded-2xl outfit'>
+          <div className='flex gap-10'>
             <li>Company</li>
             <li>Software</li>
             <li>Marketing</li>
             <li>Blogs</li>
-            <img src={logo} alt="" />
+          </div>
+
+            <img className='h-[10vh]' src={logo} alt="" />
+
+            <div className='flex gap-10'>
+            <li>Interoperability</li>
+            <li>Awards</li>
+            <li>ABDM</li>
+            <li> Career</li>
+            </div>
         </div>
+    </nav>
+
+
+{/* navbar end */}
+
+
+
     </>
   )
 }
